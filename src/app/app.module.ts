3f86e49033529550c18a35361ebfe5f71e569ce7
Fernando Robles <fernando.robles@omnitracs.com>
2020-09-04 13:22:03 -0500

    Added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 import { ListtodosComponent } from './listtodos/listtodos.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { ListtodosComponent } from './listtodos/listtodos.component';
     WelcomeComponent,
     LoginComponent,
     ErrorComponent,
-    ListtodosComponent
+    ListtodosComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
