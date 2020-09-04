6e9998d3fd18a5120e37df9d77ec33ce9f372736
Fernando Robles <fernando.robles@omnitracs.com>
2020-09-04 14:49:24 -0500

    Added menu links based on user auth

--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -8,10 +8,17 @@ export class HardcodedAuthenticationService {
   constructor() { }
 
   authenticate(username, password) {
+    console.log('before' + this.isUserLoggedIn)
     if(username==='fer' && password === 'asdf') {
+      sessionStorage.setItem('authenticatedUser',username)
       return true
     } else {
       return false;
     }
+  }
+
+  isUserLoggedIn(){
+    let user = sessionStorage.getItem('authenticatedUser')
+    return !(user === null);
   }
 }
