3f343f573d86fba1bb1cbcd84a99e9b5a21fd850
Fernando Robles <fernando.robles@omnitracs.com>
2020-09-04 15:02:18 -0500

    Implemented logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorComponent } from './error/error.component';
 import { ListtodosComponent } from './listtodos/listtodos.component';
+import { LogoutComponent } from './logout/logout.component';
 
 const routes: Routes = [
   { path:'welcome/:name', component: WelcomeComponent},
   { path: 'todos', component: ListtodosComponent},
-  { path:'login' , component: LoginComponent},
+  { path:'login' , component: LoginComponent},  
+  { path:'logout', component: LogoutComponent},
+
   { path:'**', component: ErrorComponent}
 ];
 
